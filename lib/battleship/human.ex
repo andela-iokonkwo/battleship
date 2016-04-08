59c05d4171602084396e6ae2716f48b7a67d2ec7
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-04-08 23:15:44 +0100

    feat(player): create two kind of players - human and computer

---
+++ lib/battleship/human.ex
@@ -0,0 +1,48 @@
+defmodule Battleship.Human do
+  alias Battleship.Message
+  alias Battleship.Generator
+  alias Battleship.Config
+  alias Battleship.Fleet
+
+  def generate_fleet(size, ship_count) do
+    IO.puts Message.human_fleet(ship_count)
+    ships = Enum.take(Config.ships, ship_count)
+    Fleet.without_ships(size)
+    |> fleet(ships)
+  end
+
+  def fleet(fleet, []), do: fleet
+
+  def fleet(fleet, [head| tail]) do
+    ship_position = ship_without_overlap(fleet, head)
+    Fleet.update_with_ship(fleet, ship_position, head) |> fleet(tail)
+  end
+
+  def ship_without_overlap(fleet, ship_size) do
+    ship_info = ask_for_ship_info(ship_size - 2)
+    [first_coordinate, _] = String.split(ship_info, " ")
+    list = String.to_char_list(ship_info)
+    orientation = cond do
+      Enum.at(list, 0) == Enum.at(list, 3) -> :across
+      true -> :down
+    end
+    first_cell =
+    ship_position_ = cell_from_coordinate(first_coordinate)
+                     |> Generator.positions({ ship_size, orientation })
+    if Fleet.ship_overlap?(fleet, ship_position_) do
+      IO.puts "Error: This ship overlaps with another ship. "
+      ship_without_overlap(fleet, ship_size)
+    else
+      ship_position_
+    end
+  end
+
+  def cell_from_coordinate(coordinate) do
+    String.split(coordinate, "", trim: true)
+  end
+
+  def ask_for_ship_info(ship_no) do
+    IO.gets("Enter the squares for the #{Enum.at(Config.units, ship_no)} ship: ")
+    |> String.rstrip(?\n)
+  end
+end
