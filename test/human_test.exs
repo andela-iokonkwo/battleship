7a93f23b6540f3c5c40594e8f325983851a3db58
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-04-08 23:21:28 +0100

    test(battleship): very rough test - requires a lot of fleshing out

---
+++ test/human_test.exs
@@ -0,0 +1,10 @@
+defmodule BattleshipHumanTest do
+  use ExUnit.Case
+  doctest Battleship.Human
+
+  test "layout_ships" do
+    # ships_info = Battleship.Human.generate_fleet(8, 4)
+    # IO.inspect ships_info
+    # assert  Enum.count(ships_info) == 8
+  end
+end
